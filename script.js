01c92fbc857f2b0301002e318675336053fac8f2
nevena59 <nevena.djurickovic@gmail.com>
2023-04-23 14:59:19 +0200

    Write functions

--- script.js
+++ script.js
@@ -1,40 +1,99 @@
 
 var domain = "https://my-json-server.typicode.com/nevena59/code-challenge";
-var outputDiv = document.getElementById("output");
+// var outputEmployees = document.getElementById("outputEmployees");
+// var outputSalaries = document.getElementById("outputSalaries");
+var endResult = document.getElementById("endResult");
 
 function getEmplFrom(callback)
 {
-	return new Promise((reject, resolve)=>{
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', domain + "/employees");
-		xhr.onload = function(e)
-		{
-			if (this.status == 200)
-			{
-				reject(e);
-				callback(e);
-			}
-            outputDiv.innerHTML = xhr.response;
-			resolve(e);
-		}
-
-		xhr.send();
-	});
+    return new Promise((resolve, reject)=>{
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', domain + "/employees");
+        xhr.onload = function(e)
+        {
+            if (this.status == 200)
+            {
+                // outputEmployees.innerHTML = xhr.response;
+                resolve(e);
+            } else {
+                reject(e);
+                callback(e);
+            }
+        }
+        xhr.send();
+    });
 }
 
-getEmplFrom(function() {
-    console.log('@@@@');
+function abRmnSal(callback)
+{
+    return new Promise((resolve, reject)=>{
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', domain + "/salaries");
+        xhr.onload = function(e)
+        {
+            if (this.status == 200)
+            {
+                // outputSalaries.innerHTML = xhr.response;
+                resolve(e);
+            } else {
+                reject(e);
+                callback(e);
+            }
+        }
+        xhr.send();
+    });
+}
+
+
+function mergeEmployeesAndSalaries() {
+    return Promise.all([getEmplFrom(), abRmnSal()]).then(function (values) {
+      const employees = JSON.parse(values[0].target.response);
+      const salaries = JSON.parse(values[1].target.response);
+      const mergedArray = employees.map(function (employee) {
+        const salaryObj = salaries.find(function (salary) {
+          return salary.employeeId === employee.id;
+        });
+        const salary = salaryObj ? salaryObj.salary : null;
+        return {
+          id: employee.id,
+          name: employee.name,
+          salary: salary,
+        };
+      });
+      return mergedArray;
+    });
+  }
+
+function orderBySalary(sortOrder) {
+    return mergeEmployeesAndSalaries().then(function (mergedArray) {
+        mergedArray.sort(function(a, b) {
+            if (sortOrder === 'ascending') {
+              return a.salary - b.salary;
+            } else if (sortOrder === 'descending') {
+              return b.salary - a.salary;
+            } else {
+                console.log('Wrong input');
+            }
+          });
+        return mergedArray;
+    });
+}
+
+function getEmployees(sortOrder) {
+    return orderBySalary(sortOrder).then(function(sortedArray) {
+      document.getElementById("endResult").innerHTML = JSON.stringify(sortedArray);
+      return sortedArray;
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+  
+getEmployees('ascending').then(function(result) {
+    console.log(result);
   });
 
-function abRmnSal()
-{
-	//TODO returns salaries from json
-
-}
-
-
-function getEmployees()
-{
-
-	//TODO should be exposed function which returns array of employees => for example [{id:"id", "name":"test", "salary":""}]
-}
+// getEmployees('descending').then(function(result) {
+//     console.log(result);
+//   });
+  
+  
